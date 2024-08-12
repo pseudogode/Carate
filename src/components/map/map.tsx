64486e0848e236e11df3b8b1f311acdc1e21a69a
pseudogode <jordakiev.online@gmail.com>
2024-08-12 15:08:33 +0000

    Fix leaflet types

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,8 +1,9 @@
+import { LatLngExpression } from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Map() {
 
-  const coordinates = [42.684743330511225, 23.318935591172526];
+  const coordinates:LatLngExpression = [42.684743330511225, 23.318935591172526];
 
   return (
     <MapContainer id='map' center={coordinates} zoom={16} scrollWheelZoom={true}>
