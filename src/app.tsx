dc4320b09a907eac043c91e481f246a078fcc140
pseudogode <pseudogode>
2024-09-02 10:00:41 +0000

    Restructure app layout logic

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Navigate, Outlet, Route, RouterProvider } from "react-router-dom";
 import Header from "./components/header/header";
 import classes from './app.module.scss'
-import './i18n'
 import { LanguageProvider } from "./providers/language-provider/language-provider";
+import Contacts from "./pages/contacts/contacts";
+import Home from "./pages/home/Home";
+import Market from "./pages/market/market";
+import Order from "./pages/order/order";
+
+const appLayout = (
+  <div className={classes.root}>
+    <div className={classes.header}>
+      <Header />
+    </div>
+    <div className={`${classes.section} ${classes.content}`}>
+      <Outlet />
+    </div>
+  </div>
+);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    [
+      <Route path="/" element={appLayout}>
+        <Route index element={<Home />} />
+        <Route path='market' element={<Market />} />
+        <Route path='order' element={<Order />} />
+        <Route path='contacts' element={<Contacts />} />
+      </Route>
+      ,
+      <Route path="*" element={<Navigate to="/" />} />
+    ]
+  )
+)
 
 function App() {
   return (
     <LanguageProvider>
-      <div className={classes.root}>
-        <div className={classes.header}>
-          <Header/>
-        </div> 
-        <div className={`${classes.section} ${classes.content}`}> 
-          <Outlet/>
-        </div>
-      </div>
+      <RouterProvider router={router} />
     </LanguageProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.tsx'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/home/Home.tsx'
-import Market from './pages/market/market.tsx'
-import Order from './pages/order/order.tsx'
-import Contacts from './pages/contacts/contacts.tsx'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<App/>}>
-          <Route index element={<Home/>}/>
-          <Route path='market' element={<Market/>}/>
-          <Route path='order' element={<Order/>}/>
-          <Route path='contacts' element={<Contacts/>}/>
-        </Route>
-
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
 )
